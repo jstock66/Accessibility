13a9e0d0a2100746c6ac523737998a65a84ba18e
jstock66 <jacstockman@gmail.com>
2025-07-29 15:54:17 -0400

    Accessibility
    Interactive Lesson on Accessibility

---
+++ story_content/6OzkXUlY77m_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6OzkXUlY77m_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.101.34961.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:01.504%0D%0AClick%20to%20reveal%20each%20goal.%0D%0A%0D%0A"}]})
